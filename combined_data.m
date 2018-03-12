f98b07949ebcad4a9b4f6dbbd0cc8feae9107978
nitheeshmuthuraj <32002099+nitheeshmuthuraj@users.noreply.github.com>
2018-03-12 15:28:08 -0700

    Add files via upload

---
+++ combined_data.m
@@ -0,0 +1,9 @@
+            cd outputclass
+            fopen('combined.csv','w')             
+names_class=["ABOUT","HEARING","GOOUT","FIND","FATHER","DECIDE","DEAF","COP","CAN","AND"]
+            for i =1:length(names_class)
+                temp=strcat(names_class(i),'.csv')
+                x=readtable(temp)
+                x=table2array(x)
+                dlmwrite('combined.csv',vertcat(table2array(readtable('combined.csv')),x))
+            end
